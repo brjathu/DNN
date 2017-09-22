cb1bf1a857e7fc3eaa26ba95aa8656d5f2fc4687
brjathu <brjathu@gmail.com>
2017-09-22 17:04:24 +1000

    final code

--- dnn.py
+++ dnn.py
@@ -10,19 +10,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 tf.set_random_seed(1)
 tf.logging.set_verbosity(tf.logging.ERROR)
 print(tf.__version__)
 
 
 # parameters
-LR = 0.0005
+LR = 0.001
 EPOCH = 500
-BATCH_SIZE = 200
-MODEL = 4
+BATCH_SIZE = 500
+MODEL = 5
 RESULTS_PATH = "./model/" + str(MODEL) + "/"
-LOG_FILE=open(RESULTS_PATH+'log.txt', 'a')
+LOG_FILE = open(RESULTS_PATH + 'log.txt', 'a')
 
 
 def logEntry(TMP_STRING):
@@ -33,30 +32,33 @@ def logEntry(TMP_STRING):
 
 
 def buildTestSet(fileName):
-    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime())+" **** Building Dataset")
-    X=[]
-    Y=[]
+    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime()) +
+             " **** Building Dataset")
+    X = []
+    Y = []
     with open(fileName) as file:
         for i in file:
-            c=i.rstrip()
-            y=c[-30:]
-            x=c[:-30]
-            
-            x_list=eval(x)
-            y=eval(y)
-            
+            c = i.rstrip()
+            y = c[-30:]
+            x = c[:-30]
+
+            x_list = eval(x)
+            y = eval(y)
+
             X.append(x_list)
             Y.append(y)
-            
+
     X = np.asarray(X)
-    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime())+" **** Done Building Dataset")
-    return X,Y            
+    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime()) +
+             " **** Done Building Dataset")
+    return X, Y
 
 
 tf.reset_default_graph()
 
 # tf placeholder
-tf_x = tf.placeholder(tf.float32, [None, 96*30])    # value in the range of (0, 1)
+# value in the range of (0, 1)
+tf_x = tf.placeholder(tf.float32, [None, 96 * 30])
 tf_y = tf.placeholder(tf.float32, [None, 10])
 
 # model 1. 2
@@ -69,7 +71,7 @@ tf_y = tf.placeholder(tf.float32, [None, 10])
 # hidden_layer7 = tf.layers.dense(hidden_layer6, 10, tf.nn.relu)
 
 
-## model 3 
+# model 3
 # hidden_layer1 = tf.layers.dense(tf_x, 5000, tf.nn.relu)
 # hidden_layer2 = tf.layers.dense(hidden_layer1, 2000, tf.nn.relu)
 # hidden_layer3 = tf.layers.dense(hidden_layer2, 1000, tf.nn.relu)
@@ -80,25 +82,23 @@ tf_y = tf.placeholder(tf.float32, [None, 10])
 # hidden_layer8 = tf.layers.dense(hidden_layer7, 10, tf.nn.relu)
 
 
-## model 4
-hidden_layer1 = tf.layers.dense(tf_x, 2000, tf.nn.relu, name= "hidden_layer1")
-dropout1 = tf.layers.dropout(hidden_layer1, rate=0.5, name= "dropout1")
-hidden_layer2 = tf.layers.dense(dropout1, 1000, tf.nn.relu, name= "hidden_layer2")
-dropout2 = tf.layers.dropout(hidden_layer2, rate=0.5, name= "dropout2")
-hidden_layer3 = tf.layers.dense(dropout2, 500, tf.nn.relu, name= "hidden_layer3")
-dropout3 = tf.layers.dropout(hidden_layer3, rate=0.5, name= "dropout3")
-hidden_layer4 = tf.layers.dense(dropout3, 200, tf.nn.relu, name= "hidden_layer4")
-dropout4 = tf.layers.dropout(hidden_layer4, rate=0.5, name= "dropout4")
-hidden_layer5 = tf.layers.dense(dropout4, 100, tf.nn.relu, name= "hidden_layer5")
-hidden_layer6 = tf.layers.dense(hidden_layer5, 50, tf.nn.relu, name= "hidden_layer6")
-hidden_layer7 = tf.layers.dense(hidden_layer6, 10, tf.nn.relu, name= "hidden_layer7")
+# model 4
+hidden_layer1 = tf.layers.dense(tf_x, 2500, tf.nn.tanh, name="hidden_layer1")
+hidden_layer2 = tf.layers.dense(hidden_layer1, 2000, tf.nn.tanh, name="hidden_layer2")
+hidden_layer3 = tf.layers.dense(hidden_layer2, 1500, tf.nn.tanh, name="hidden_layer3")
+# dropout3 = tf.layers.dropout(hidden_layer3, rate=0.5, name= "dropout3")
+hidden_layer4 = tf.layers.dense(hidden_layer3, 1000, tf.nn.tanh, name="hidden_layer4")
+# dropout4 = tf.layers.dropout(hidden_layer4, rate=0.5, name= "dropout4")
+hidden_layer5 = tf.layers.dense(hidden_layer4, 500, tf.nn.tanh, name="hidden_layer5")
+hidden_layer6 = tf.layers.dense(hidden_layer5, 200, tf.nn.tanh, name="hidden_layer6")
+hidden_layer7 = tf.layers.dense(hidden_layer6, 50, tf.nn.tanh, name="hidden_layer7")
+hidden_layer8 = tf.layers.dense(hidden_layer7, 10, tf.nn.tanh, name="hidden_layer8")
 
-softmax_layer = tf.nn.softmax(hidden_layer7, name = "softmax")
+softmax_layer = tf.nn.softmax(hidden_layer8, name="softmax")
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = softmax_layer, labels = tf_y)) 
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=softmax_layer, labels=tf_y))
 # loss = tf.losses.mean_squared_error(labels=tf_y, predictions=softmax_layer)
 train = tf.train.AdamOptimizer(LR).minimize(loss)
-
 
 
 # prepare data
@@ -106,7 +106,7 @@ train = tf.train.AdamOptimizer(LR).minimize(loss)
 # x_train = np.array(x_train)
 # y_train = np.array(y_train)
 
-# #preprocessing 
+# #preprocessing
 # X = []
 # Y =[]
 # for i in range(x_train.shape[0]):
@@ -120,27 +120,27 @@ train = tf.train.AdamOptimizer(LR).minimize(loss)
 x = np.load("./data/x_train.npy")
 y = np.load("./data/y_train.npy")
 
-ratio = int(x.shape[0]*80/100)
-x_train = x[:ratio,:]
-y_train = y[:ratio,:]
+ratio = int(x.shape[0] * 80 / 100)
+x_train = x[:ratio, :]
+y_train = y[:ratio, :]
 print(x_train.shape)
 print(y_train.shape)
 
 loss_graph = []
 with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-	saver = tf.train.Saver()
-	for epoch in range(EPOCH):
-		for i in range(int(x_train.shape[0]/BATCH_SIZE)):
-			_, loss_val = sess.run([train, loss], {tf_x: np.reshape(x_train[i*BATCH_SIZE:(i+1)*BATCH_SIZE],(BATCH_SIZE,2880)) , tf_y:np.reshape(y_train[i*BATCH_SIZE:(i+1)*BATCH_SIZE],(BATCH_SIZE,10))})
-			
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    for epoch in range(EPOCH):
+        loss_avg = 0
+        for i in range(int(x_train.shape[0] / BATCH_SIZE)):
+            _, loss_val = sess.run([train, loss], {tf_x: np.reshape(x_train[i * BATCH_SIZE:(i + 1) * BATCH_SIZE], (
+                BATCH_SIZE, 2880)), tf_y: np.reshape(y_train[i * BATCH_SIZE:(i + 1) * BATCH_SIZE], (BATCH_SIZE, 10))})
+            loss_avg += loss_val
+        print(loss_avg / i)
+        loss_graph.append(loss_avg / i)
+        np.save("./model/" + str(MODEL) + "/train.npy", np.array(loss_graph))
+        plt.plot(np.array(loss_graph))
+        plt.savefig("./model/" + str(MODEL) + "/loss_graph.png")
+        # plt.show()
 
-		print(loss_val)
-		loss_graph.append(loss_val)
-		np.save("./model/" + str(MODEL) + "/train.npy",np.array(loss_graph))
-		plt.plot(np.array(loss_graph))
-		plt.savefig("./model/" + str(MODEL) + "/loss_graph.png")
-		# plt.show()
-
-		saver.save(sess, "model/" + str(MODEL) + "/ckpt/dnn.ckpt")
-
+        saver.save(sess, "model/" + str(MODEL) + "/ckpt/dnn.ckpt")
--- dnn_test.py
+++ dnn_test.py
@@ -20,9 +20,9 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 LR = 0.0005
 EPOCH = 100
 BATCH_SIZE = 200
-MODEL = 3
+MODEL = 5
 RESULTS_PATH = "./model/1/"
-LOG_FILE=open(RESULTS_PATH+'log.txt', 'a')
+LOG_FILE = open(RESULTS_PATH + 'log.txt', 'a')
 
 
 def logEntry(TMP_STRING):
@@ -33,41 +33,41 @@ def logEntry(TMP_STRING):
 
 
 def buildTestSet(fileName):
-    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime())+" **** Building Dataset")
-    X=[]
-    Y=[]
+    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime()) + " **** Building Dataset")
+    X = []
+    Y = []
     with open(fileName) as file:
         for i in file:
-            c=i.rstrip()
-            y=c[-30:]
-            x=c[:-30]
-            
-            x_list=eval(x)
-            y=eval(y)
-            
+            c = i.rstrip()
+            y = c[-30:]
+            x = c[:-30]
+
+            x_list = eval(x)
+            y = eval(y)
+
             X.append(x_list)
             Y.append(y)
-            
-    X = np.asarray(X)
-    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime())+" **** Done Building Dataset")
-    return X,Y            
 
-def plotConfusionMatrix(predictions,y,i,FILENAME,TITLE):
+    X = np.asarray(X)
+    logEntry(strftime("%Y-%m-%d %H:%M:%S", localtime()) + " **** Done Building Dataset")
+    return X, Y
+
+
+def plotConfusionMatrix(predictions, y, i, FILENAME, TITLE):
     LABELS = ['User 1', 'User 2', 'User 3', 'User 4', 'User 5', 'User 6', 'User 7', 'User 8', 'User 9', 'User 10']
     max_test = np.argmax(y, axis=1)
     max_predictions = np.argmax(predictions, axis=1)
     confusion_matrix = metrics.confusion_matrix(max_test, max_predictions)
-    fig=plt.figure(figsize=(16, 14))
-    sns.heatmap(confusion_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
-    plt.title("Confusion matrix: "+TITLE)
+    fig = plt.figure(figsize=(16, 14))
+    sns.heatmap(confusion_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d")
+    plt.title("Confusion matrix: " + TITLE)
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    fig.savefig("./model/" + str(MODEL) + "/"+FILENAME)
+    fig.savefig("./model/" + str(MODEL) + "/" + FILENAME)
     plt.clf()
-    plt.close('all') 
+    plt.close('all')
 
     return
-
 
 
 print("Start")
@@ -79,10 +79,10 @@ print("Model Loading Done")
 tf.get_default_graph().as_graph_def()
 
 input_ph = sess.graph.get_tensor_by_name("Placeholder:0")
-output = sess.graph.get_tensor_by_name("Softmax:0")
+output = sess.graph.get_tensor_by_name("softmax:0")
 
 
-LOGDIR='LOG'
+LOGDIR = 'LOG'
 train_writer = tf.summary.FileWriter(LOGDIR)
 train_writer.add_graph(sess.graph)
 
@@ -91,23 +91,23 @@ for op in sess.graph.get_operations():
 
 
 # load data
-x = np.load("./data/x_valid.npy")
-y = np.load("./data/y_valid.npy")
+x = np.load("./data/x_train.npy")
+y = np.load("./data/y_train.npy")
 print(x.shape)
 print(y.shape)
 
-ratio = int(x.shape[0]*80/100)
-x_train = x[:ratio,:]
-y_train = y[:ratio,:]
+ratio = int(x.shape[0] * 80 / 100)
+x_train = x[:ratio, :]
+y_train = y[:ratio, :]
 
-x_test = x[ratio:,:]
-y_test = y[ratio:,:]
+x_test = x[ratio:, :]
+y_test = y[ratio:, :]
 print(y_test)
 predictions = []
 for i in range(x_test.shape[0]):
-    result = sess.run([output], feed_dict={input_ph: np.reshape(x_test[i],(1,-1))})
+    result = sess.run([output], feed_dict={input_ph: np.reshape(x_test[i], (1, -1))})
     predictions.append(result[0][0])
     print(i)
 
 print(np.array(predictions).shape)
-plotConfusionMatrix(list(predictions), y_test,100, "confusion_matrix.png" , "model1")
+plotConfusionMatrix(list(predictions), y_test, 100, "confusion_matrix.png", "model1")
